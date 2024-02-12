c0c5e59a063c0d40c75e4c0afccaa7cf05292272
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-02-12 20:57:13 +0200

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,2 +1,30 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.getElementById('datetime-picker'),
+  button: document.querySelector('[data-start]'),
+  day: document.querySelector('[data-days]'),
+  hour: document.querySelector('[data-hours]'),
+  minute: document.querySelector('[data-minutes]'),
+  second: document.querySelector('[data-seconds]'),
+};
+
+let selectedDate;
+let intervID = null;
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  addContent({ days, hours, minutes, seconds }); 
+  timerOff();
+   
+};
